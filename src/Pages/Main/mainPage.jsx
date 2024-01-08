57ddfff4fe37b1c479ff77bcb68af32e0821440f
metelkov <al.metelkov@gmail.com>
2024-01-08 21:01:53 +0300

    starting main page

--- src/Pages/Main/mainPage.jsx
+++ src/Pages/Main/mainPage.jsx
@@ -5,8 +5,31 @@ import classes from "../Main/styles.module.css";
 export const MainPage = () => {
     return (
         <div className={classes.mainPageWrapper}>
-          
+            <div className={classes.sliderSpot}>
 
+            </div>
+            <div className={classes.cardPlace}>
+                <div className={classes.cardPlaceLeft}>
+
+                </div>
+                <div className={classes.cardPlaceCenter}>
+
+                </div>
+                <div className={classes.cardPlaceRight}>
+
+                </div>
+            </div>
+            <div className={classes.newGoods}>
+                <div className={classes.newGoodsLeft}>
+
+                </div>
+                <div className={classes.newGoodsCenter}>
+
+                </div>
+                <div className={classes.newGoodsRight}>
+
+                </div>
+            </div>
         </div>
 
     )
