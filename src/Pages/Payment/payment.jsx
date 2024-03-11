305a8b99fa97927178540ac230d8afa755ff02e3
Metelkov <al.metelkov@gmail.com>
2024-03-12 02:03:38 +0300

    try Lun

--- src/Pages/Payment/payment.jsx
+++ src/Pages/Payment/payment.jsx
@@ -2,28 +2,64 @@ import React from "react";
 import classes from "./styles.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { number, object } from "yup";
+import { InputMask, useMask } from "@react-input/mask";
 
 const schema = object().shape({
-  firstOctetCard: number().max(4).min(1).default(undefined),
+  firstOctetCard: number().default(undefined),
 });
 
 export const Payment = () => {
+  const inputRef = useMask({
+    mask: "____ ____ ____ ____",
+    replacement: { _: /\d/ },
+  });
+
   return (
     <div className={classes.paymentWrap}>
       <div className={classes.paymentTittle}>
         <p>Оплатить</p>
       </div>
-      <div className={classes.paymentCardInfo}>
-        <p>visa</p>
+      <div id="cardIdentificatorShow" className={classes.paymentCardInfo}>
+        <p></p>
       </div>
       <div className={classes.paymentFormikWrap}>
         <Formik
           validationSchema={schema}
-          initialValues={{ schema.default() }}
+          initialValues={schema.default()}
           className={classes.paymentFormicStyle}
+          onSubmit={(valOfInput) => {
+            const valOfInputToStr = Object.values(valOfInput) //вероятно нужно куда то вынести и тут не писать и вообще создать массивы по которым будут определяться значения
+              .join() //оказалось - переписал по своему
+              .replaceAll(" ", "");
+            if (valOfInputToStr.length != 16) {
+              console.log("не все цифры введены");
+              document.getElementById("submitbtn").disabled = true; //а обратно не вернем - нужно отслеживать в реальн времени
+            }
+
+            if (valOfInputToStr.substr(1, 1) == 1 || 2) {
+              console.log("avia");
+              const div = document.createElement("div"); //название карты дуюдируется, не стирается при нажатие на Оплатить
+              div.innerHTML = "Avia";
+              document.getElementById("cardIdentificatorShow").append(div);
+            }
+            if (valOfInputToStr.substr(1, 1) == 3) {
+              const div = document.createElement("div");
+              div.innerHTML = "American Express";
+              document.getElementById("cardIdentificatorShow").append(div);
+              console.log("American Express");
+            }
+            if (valOfInputToStr.substr(1, 1) == 4) {
+              const div = document.createElement("div");
+              div.innerHTML = "visa";
+              document.getElementById("cardIdentificatorShow").append(div);
+              console.log("visa");
+            }
+            // console.log(valOfInputToStr);
+          }}
         >
           <Form className={classes.paymentFormWrap}>
             <Field
+              innerRef={inputRef}
               type="text"
               id="firstOctetCard"
               name="firstOctetCard"
@@ -35,7 +71,9 @@ export const Payment = () => {
               className={classes.paymentOctetErrorMessage}
             />
 
-            <button type="submit">Оплатить</button>
+            <button id="submitbtn" type="submit">
+              Оплатить
+            </button>
             <button type="reset">Сбросить</button>
           </Form>
         </Formik>
