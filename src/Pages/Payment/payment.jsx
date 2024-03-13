715304aa141186990178b881569f8e2fd577ce06
Metelkov <al.metelkov@gmail.com>
2024-03-13 21:58:07 +0300

    payment

--- src/Pages/Payment/payment.jsx
+++ src/Pages/Payment/payment.jsx
@@ -28,34 +28,36 @@ export const Payment = () => {
           initialValues={schema.default()}
           className={classes.paymentFormicStyle}
           onSubmit={(valOfInput) => {
-            const valOfInputToStr = Object.values(valOfInput) //вероятно нужно куда то вынести и тут не писать и вообще создать массивы по которым будут определяться значения
-              .join() //оказалось - переписал по своему
-              .replaceAll(" ", "");
-            if (valOfInputToStr.length != 16) {
-              console.log("не все цифры введены");
-              document.getElementById("submitbtn").disabled = true; //а обратно не вернем - нужно отслеживать в реальн времени
-            }
-
-            if (valOfInputToStr.substr(1, 1) == 1 || 2) {
-              console.log("avia");
-              const div = document.createElement("div"); //название карты дуюдируется, не стирается при нажатие на Оплатить
-              div.innerHTML = "Avia";
-              document.getElementById("cardIdentificatorShow").append(div);
-            }
-            if (valOfInputToStr.substr(1, 1) == 3) {
-              const div = document.createElement("div");
-              div.innerHTML = "American Express";
-              document.getElementById("cardIdentificatorShow").append(div);
-              console.log("American Express");
-            }
-            if (valOfInputToStr.substr(1, 1) == 4) {
-              const div = document.createElement("div");
-              div.innerHTML = "visa";
-              document.getElementById("cardIdentificatorShow").append(div);
-              console.log("visa");
-            }
-            // console.log(valOfInputToStr);
+            console.log(valOfInput);
           }}
+          //   const valOfInputToStr = Object.values(valOfInput) //вероятно нужно куда то вынести и тут не писать и вообще создать массивы по которым будут определяться значения
+          //     .join() //оказалось - переписал по своему
+          //     .replaceAll(" ", "");
+          //   if (valOfInputToStr.length != 16) {
+          //     console.log("не все цифры введены");
+          //     document.getElementById("submitbtn").disabled = true; //а обратно не вернем - нужно отслеживать в реальн времени
+          //   }
+
+          //   if (valOfInputToStr.substr(1, 1) == 1 || 2) {
+          //     console.log("avia");
+          //     const div = document.createElement("div"); //название карты дуюдируется, не стирается при нажатие на Оплатить
+          //     div.innerHTML = "Avia";
+          //     document.getElementById("cardIdentificatorShow").append(div);
+          //   }
+          //   if (valOfInputToStr.substr(1, 1) == 3) {
+          //     const div = document.createElement("div");
+          //     div.innerHTML = "American Express";
+          //     document.getElementById("cardIdentificatorShow").append(div);
+          //     console.log("American Express");
+          //   }
+          //   if (valOfInputToStr.substr(1, 1) == 4) {
+          //     const div = document.createElement("div");
+          //     div.innerHTML = "visa";
+          //     document.getElementById("cardIdentificatorShow").append(div);
+          //     console.log("visa");
+          //   }
+          //   console.log(valOfInputToStr);
+          // }}
         >
           <Form className={classes.paymentFormWrap}>
             <Field
@@ -65,7 +67,7 @@ export const Payment = () => {
               name="firstOctetCard"
               className={classes.paymentOctet}
             />
-            {({ field, meta }) => (
+            {({ field, form: { setError }, meta }) => (
               <div>
                 <input
                   {...field}
